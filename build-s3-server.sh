1998f6cddc439302f6da5c34fea07087efab0c7c
Tom Harris <tom.harris@12kites.com>
2022-09-27 16:48:31 +0100

    Change build script

--- build-s3-server.sh
+++ build-s3-server.sh
@@ -39,28 +39,6 @@ if [ $RESULT -eq 0 ]; then
     cd ../$VUEDIR
     npm run clean
 
-##copy umd and css files over
-cd ./s3-server
-
-rm -rf files/files
-mkdir -p files/files
-
-cp ../dist/txmanager.umd.js files/files
-cp ../dist/txmanager.umd.min.js files/files
-cp ../dist/*.css files/files
-touch files/files/txmanager.css
-
-#create the scripts.json file, add the content and set the version number?
-rm ./files/feature-spec.json
-cp ./spec-source.json ./files/feature-spec.json
-sed -i '' -e "s/%%VER%%/$VERSION/g" ./files/feature-spec.json
-
-#build docker image
-# LOCALTAG="ew-feature-txmanager"
-# BASE="ew-feature-txmanager"
-# docker build -t $LOCALTAG .
-# docker tag $LOCALTAG registry.digitalocean.com/eport/$BASE:dev-latest
-# docker push registry.digitalocean.com/eport/$BASE:dev-latest
 else
   echo Build failed
 fi
